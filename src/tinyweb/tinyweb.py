75bc875a6bc4ecb72b7a5bd9afe2abb419ee383e
haines zhang <zhmocean@gmail.com>
2013-11-04 18:23:48 +0800

    first commit

---
+++ src/tinyweb/tinyweb.py
@@ -0,0 +1,26 @@
+import web
+class s_index:
+	def GET(self):
+		render = web.template.render("template/")
+		return render.home("hello")
+
+class s_api:
+	def GET(self):
+		return "this is my: "+str(globals()['schedule'].monitor('monitor1').myName())
+
+class TinyWeb:
+	def __init__(self):
+		self.app = web.application(self.__urls(), globals(), True)
+
+	def __urls(self):
+		return (
+			'/','s_index',
+			'/api', 's_api'
+		)
+
+	def start(self):
+		self.app.run()
+
+if __name__ == "__main__": 
+	w = TinyWeb()
+	w.start()
